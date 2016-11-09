5c333c0d9598d60392f68b9dc1819d48c0a461fc
Patrick Paysant <patrick.paysant@linagora.com>
2016-11-09 10:51:11 +0100

    Fix syntax error

--- hook/gtuhooks.php
+++ hook/gtuhooks.php
@@ -25,52 +25,51 @@ use OCP\AppFramework\Http\JSONResponse;
 
 class GtuHooks {
 
-	var $userGtuValidationMapper;
-	var $session;
+    var $userGtuValidationMapper;
+    var $session;
 
-	public function  __construct($userGtuValidationMapper, $session) {
-		$this->userGtuValidationMapper = $userGtuValidationMapper;
-		$this->session = $session;
-	}
+    public function  __construct($userGtuValidationMapper, $session) {
+        $this->userGtuValidationMapper = $userGtuValidationMapper;
+        $this->session = $session;
+    }
 
-	/**
-	* post_deleteUser
-	*/
-	public function onPostDeleteUser($user) {
-		\OCP\Util::writeLog('GtuHooks', 'onPostDeleteUser ',\OCP\Util::INFO);
-		$uid = $user->getUID();
-		try {
-			$ugv = $this->userGtuValidationMapper->findByUid($uid);
-		}
-		catch(\Exception $e) {
-			return new JSONResponse([
-				'status' => 'success',
+    /**
+    * post_deleteUser
+    */
+    public function onPostDeleteUser($user) {
+        \OCP\Util::writeLog('GtuHooks', 'onPostDeleteUser ',\OCP\Util::INFO);
+        $uid = $user->getUID();
+        try {
+            $ugv = $this->userGtuValidationMapper->findByUid($uid);
+        }
+        catch(\Exception $e) {
+            return new JSONResponse([
+                'status' => 'success',
                 'data' => [
                     'message' => '',
-                ]);
-		}
-		$this->userGtuValidationMapper->delete($ugv);
-		return new JSONResponse([
-				'status' => 'success',
+                ],
+            ]);
+        }
+        $this->userGtuValidationMapper->delete($ugv);
+        return new JSONResponse([
+                'status' => 'success',
                 'data' => [
                     'message' => '',
-                ]);
-	}
+                ],
+            ]);
+    }
 
-	public function onLogout() {
-		$this->session->remove('gtu_ok');
-	}
+    public function onLogout() {
+        $this->session->remove('gtu_ok');
+    }
 
-	public function register($session) {
-		$session->listen('\OC\User', 'postDelete', function ($user) {
-			$this->onPostDeleteUser($user);
-		});
-		$session->listen('\OC\User', 'logout', function () {
-			$this->onLogout();
-		});
-	}
-
-
-
+    public function register($session) {
+        $session->listen('\OC\User', 'postDelete', function ($user) {
+            $this->onPostDeleteUser($user);
+        });
+        $session->listen('\OC\User', 'logout', function () {
+            $this->onLogout();
+        });
+    }
 }
 
